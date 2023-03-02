36856672768ecfe4f2c7ba61e05f85457bda3d13
bienfait shomari <bienfaitshm@gmail.com>
2023-03-02 10:02:41 +0100

    set stack screen navigation

---
+++ app/choice_profile.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "native-base";
+
+export default function WelecomeScreen() {
+  return (
+    <Box>
+      <Text>Welecome Screen</Text>
+    </Box>
+  );
+}
---
+++ app/welcome.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "native-base";
+
+export default function Welcome() {
+  return (
+    <Box>
+      <Text>Bonjour welcome</Text>
+    </Box>
+  );
+}
