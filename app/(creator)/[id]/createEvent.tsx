fe71618c9e6d4a44037fb1c2db800eb5cab9ace6
bienfait shomari <bienfaitshm@gmail.com>
2023-04-13 21:36:04 +0200

    remove post action and add submiter

--- app/(creator)/[id]/createEvent.tsx
+++ app/(creator)/[id]/createEvent.tsx
@@ -3,40 +3,18 @@ import { Stack, useRouter } from "expo-router";
 import CreatorEvent, { DataInputType } from "../../../components/CreatorEvent";
 import { useParamsID } from "./creatorUtils";
 import { usePostEvent } from "../../../hooks/apis";
-import { useToastAction } from "../../../hooks/useToastAction";
 import { useSubmiter } from "../../../hooks/useSubmiter";
-
-const usePostAction = (category: string | number) => {
-    const router = useRouter();
-    const toast = useToastAction();
-    const mutation = usePostEvent();
-    const submiter = useSubmiter<DataInputType, any>({
-        mutate: (values) => mutation.mutate({ category, ...values }),
-    });
-    return (value: DataInputType, callback?: (state: boolean) => void) => {
-        mutation.mutate(
-            {
-                category,
-                ...value,
-            },
-            {
-                onError(error) {
-                    callback?.(false);
-                    toast.toastError(JSON.stringify(error, null, 4));
-                },
-                onSuccess() {
-                    callback?.(false);
-                    toast.toastSuccess();
-                    router.back();
-                },
-            }
-        );
-    };
-};
 
 export default function CreateEventProps() {
     const category = useParamsID();
-    const handlerSubmit = usePostAction(category);
+    const router = useRouter();
+    const mutation = usePostEvent();
+    const submiter = useSubmiter<DataInputType, any>({
+        mutate: (values) => mutation.mutate({ category, ...values }),
+        options: {
+            onSuccess: router.back,
+        },
+    });
     return (
         <>
             <Stack.Screen
@@ -50,7 +28,7 @@ export default function CreateEventProps() {
                     },
                 }}
             />
-            <CreatorEvent onSubmit={handlerSubmit} />
+            <CreatorEvent isLoading={mutation.isLoading} onSubmit={submiter} />
         </>
     );
 }
